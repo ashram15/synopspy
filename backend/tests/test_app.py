762ffc9d7d4ee8d55affa72793c18868ca91c741
Aishwarya Ramesh <ashram1015@gmail.com>
2025-11-26 14:28:25 -0600

    removed comments

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -1,11 +1,9 @@
 from fastapi.testclient import TestClient
-from app import app  # Make sure imports match your structure
+from app import app
 
 client = TestClient(app)
 
 
 def test_read_main():
-    # This proves your app starts up without crashing
     response = client.get("/")
-    # If you don't have a root route, you can test a 404 is returned, which is still valid!
     assert response.status_code in [200, 404]
